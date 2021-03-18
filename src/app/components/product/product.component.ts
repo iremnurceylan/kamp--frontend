096a705e8db60de4c3805219e65b371927419784
irem nur ceylan <ceylannnirem@outlook.com>
2021-03-18 21:28:44 +0300

    17 Mart Ders Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,9 +14,10 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded = false;
+  filterText ="";
  
 
-  constructor(private productSerive:ProductService,private activatedRoute:ActivatedRoute) { }
+  constructor(private productSerive:ProductService,private activatedRoute:ActivatedRoute,private toastrService:ToastrService,private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -43,5 +46,15 @@ export class ProductComponent implements OnInit {
     })
     
   }
+
+  addToCart(product:Product){
+    if(product.productId==1){
+      this.toastrService.error("Hata" , "Ürün sepete eklenemez");
+    }else{
+    this.toastrService.success("Sepete eklendi",product.productName)
+    this.cartService.addToCart(product);
+    }
+
+}
 
 }
