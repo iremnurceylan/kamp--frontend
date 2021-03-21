44324efe1c6db5ae120cf56d9dcc66958f7eaaa6
irem nur ceylan <ceylannnirem@outlook.com>
2021-03-22 00:36:27 +0300

    20 Mart Ders Sonu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import { from, Observable } from 'rxjs';
 import {ListResponseModel} from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,10 @@ export class ProductService {
       return this.httpClient.get<ListResponseModel<Product>>(newPath);
       }
 
-  }
+    add(product:Product):Observable<ResponseModel>{
+      return this.httpClient.post<ResponseModel>(this.apiUrl + "products/add" , product)
+      }
+      }
+
+  
 
